1f2c458dcf72e21e88d8a56b97e27acc92d40a8a
Shana2002 <hansakaravi02@gmail.com>
2025-01-17 15:34:37 +0530

    login verification done

--- app/src/main/java/com/example/citycycle/ui/LoginActivity.java
+++ app/src/main/java/com/example/citycycle/ui/LoginActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -32,6 +34,10 @@ public class LoginActivity extends AppCompatActivity {
 //        varible decleration
         TextView sign_button = findViewById(R.id.sign_button);
         TextView login_button = findViewById(R.id.login_btn);
+        EditText email = findViewById(R.id.email);
+        EditText passwrod = findViewById(R.id.password);
+        String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
 //        sing button function
         sign_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +51,12 @@ public class LoginActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (email.getText().toString().isEmpty() || passwrod.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "All field required", Toast.LENGTH_SHORT).show();
+                } else if (!email.getText().toString().matches(emailPattern)) {
+                    Toast.makeText(LoginActivity.this, "All field required", Toast.LENGTH_SHORT).show();
+                }
+
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             }
         });
